76c754e38470beae8d37e4daa59e5d7350afa8a8
ojeanfeli_pe <jean.felipe.moreira12@gmail.com>
2025-11-21 17:37:53 -0300

    utilizando um loop foreach para percorrer os horários e renderizá-los na página

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,17 +2,29 @@ import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad( {date} ){
     const opening = openingHours.map((hour) => {
         //Recupera somente a hora.
-        const [schedulesHour] = hour.split(":")
+        const [scheduleHour] = hour.split(":")
 
         //Adiciona a hora na date e verifica se está no passado
-        const isHourPast = dayjs(date).add(schedulesHour,"hour").isAfter(dayjs())
+        const isHourPast = dayjs(date).add(scheduleHour,"hour").isAfter(dayjs())
         
         return {
             hour,
-            avaiable: isHourPast,
+            available: isHourPast,
         }
+    })
+
+    //Renderizar os horários
+    opening.forEach(( {hour,available} ) =>{
+        const li = document.createElement("li")
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+        hours.append(li)
     })
 }
