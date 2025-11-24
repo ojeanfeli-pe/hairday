3e637bc5a2ba9d9f3b2f4c896049a4f6b018c195
ojeanfeli_pe <jean.felipe.moreira12@gmail.com>
2025-11-24 15:09:52 -0300

    Corrigindo alguns erros

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,9 +1,10 @@
 import {schedulesDay} from "./load.js"
-import { scheduleCancel } from "../../services/schedule-cancel"
+import { scheduleCancel } from "../../services/schedule-cancel.js"
+
 const periods = document.querySelectorAll(".period")
 
 //Gera evento de click para cada lista (manhã, tarde e noite)
-periods.forEach((periods) => {
+periods.forEach((period) => {
     //Captura o evento de clique na lista
     period.addEventListener("click", async (event)=>{
         if(event.target.classList.contains("cancel-icon")){
@@ -17,15 +18,17 @@ periods.forEach((periods) => {
             if(id){
                 //Confirma se o usuário quer cancelar
                 const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?")
+
+                if(isConfirm){
+                    //Faz a requisição na API para cancelar.
+                    await scheduleCancel({id})
+                    
+                    // Recarrega os agendamentos
+                    schedulesDay()
+                }
             }
 
-            if(isConfirm){
-                //Faz a requisição na API Para cancelar.
-               await scheduleCancel({id})
-
-               //Recarrega os agendamentos
-               schedulesDay()
-            }
+            
         }
     })
 })
