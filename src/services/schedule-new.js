cb4b603e273f9c61ecea32b6eaebdeadf163cf86
ojeanfeli_pe <jean.felipe.moreira12@gmail.com>
2025-11-24 13:55:13 -0300

    serviço para buscar agendamentos do dia

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,6 +2,7 @@ import {apiConfig} from "./api-config.js"
 
 export async function scheduleNew({id, name, when}){
     try {
+        //Faz requisição para enviar os dados do agendamento
         await fetch(`${apiConfig.baseURL}/schedules`, {
             method: 'POST',
             headers: {
@@ -9,7 +10,8 @@ export async function scheduleNew({id, name, when}){
             },        
             body: JSON.stringify({id, name, when}),
         })
-
+        
+        //Exibe mensagem de agendamento realizado
     alert("Agendamento realizado com sucesso!")
     } catch (error) {
         console.log(error)
